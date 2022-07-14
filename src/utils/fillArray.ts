80f7352e4d3fe41c336ca64e6feabef01fcc4a8d
NM-va <nad.n.mwa@gmail.com>
2022-07-14 18:24:20 +0300

    change starting number

--- src/utils/fillArray.ts
+++ src/utils/fillArray.ts
@@ -2,7 +2,7 @@ export const fillArray = (countElements: number): number[] => {
   const arr: number[] = [];
 
   if (countElements) {
-    let i = 0;
+    let i = 1;
 
     while (i <= countElements) {
       arr.push(i);
