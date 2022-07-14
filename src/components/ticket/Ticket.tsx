b3fdc68046453a292aac673098254b196d1a601e
NM-va <nad.n.mwa@gmail.com>
2022-07-15 02:06:43 +0300

    create functions selected elements

--- src/components/ticket/Ticket.tsx
+++ src/components/ticket/Ticket.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { ImMagicWand } from 'react-icons/im';
 
@@ -7,8 +7,59 @@ import { fillArray } from '../../utils/fillArray';
 import { Cell } from './Cell';
 
 export const Ticket = (): any => {
+  const [selectedElementsFirstFiled, setSelectedElementsFirstFiled] = useState<number[]>(
+    [],
+  );
+  const [selectedElementsSecondFiled, setSelectedElementsSecondFiled] = useState<
+    number[]
+  >([]);
+
+  const MAX_COUNT_SELECTED_FIRST_FIELD = 8;
+  const MAX_COUNT_SELECTED_SECOND_FIELD = 2;
   const NUMBER_CELL_FIRST_FIELD = 19;
   const NUMBER_CELL_SECOND_FIELD = 2;
+
+  const toggleSelectedCellFirstField = (numberCell: number): void => {
+    const isSelected = selectedElementsFirstFiled.includes(numberCell);
+
+    if (
+      selectedElementsFirstFiled.length === MAX_COUNT_SELECTED_FIRST_FIELD &&
+      !isSelected
+    ) {
+      return;
+    }
+
+    if (isSelected) {
+      const arrSelectedElements = selectedElementsFirstFiled.filter(
+        selectedNumber => selectedNumber !== numberCell,
+      );
+
+      setSelectedElementsFirstFiled(arrSelectedElements);
+    } else {
+      setSelectedElementsFirstFiled([...selectedElementsFirstFiled, numberCell]);
+    }
+  };
+
+  const toggleSelectedCellSecondField = (numberCell: number): void => {
+    const isSelected = selectedElementsSecondFiled.includes(numberCell);
+
+    if (
+      selectedElementsSecondFiled.length === MAX_COUNT_SELECTED_SECOND_FIELD &&
+      !isSelected
+    ) {
+      return;
+    }
+
+    if (isSelected) {
+      const arrSelectedElements = selectedElementsSecondFiled.filter(
+        selectedNumber => selectedNumber !== numberCell,
+      );
+
+      setSelectedElementsSecondFiled(arrSelectedElements);
+    } else {
+      setSelectedElementsSecondFiled([...selectedElementsSecondFiled, numberCell]);
+    }
+  };
 
   const arrayNumbersFirstField = fillArray(NUMBER_CELL_FIRST_FIELD);
   const arrayNumbersSecondField = fillArray(NUMBER_CELL_SECOND_FIELD);
@@ -26,15 +77,25 @@ export const Ticket = (): any => {
           <span className="textBold">Поле 1</span> Отметьте 8 чисел.
         </div>
         {arrayNumbersFirstField.map(number => (
-          <Cell numberCell={number} key={`${number} "nad"`} />
+          <Cell
+            numberCell={number}
+            key={`${number} "nad"`}
+            changeSelectedCell={() => toggleSelectedCellFirstField(number)}
+            isSelectedCell={selectedElementsFirstFiled.includes(number)}
+          />
         ))}
       </div>
       <div className="fieldSecond">
         <div className="titleField">
-          <span className="textBold">Поле 2</span> Отметьте 2 числа.
+          <span className="textBold">Поле 2</span> Отметьте 1 число.
         </div>
         {arrayNumbersSecondField.map(number => (
-          <Cell numberCell={number} key={`${number} "sss"`} />
+          <Cell
+            numberCell={number}
+            key={`${number} "nad"`}
+            changeSelectedCell={() => toggleSelectedCellSecondField(number)}
+            isSelectedCell={selectedElementsSecondFiled.includes(number)}
+          />
         ))}
       </div>
       <div className="btnBox">
