6d772cc283cd663a7e947e98e651f72123569ace
NM-va <nad.n.mwa@gmail.com>
2022-07-17 19:31:14 +0300

    add result component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.scss';
+import { Result } from './components/result/Result';
 import { Ticket } from './components/ticket/Ticket';
 
 const App = (): any => {
+  const [isTicketWon, setIsTicketWon] = useState<boolean>(false);
+
   return (
     <div className="App">
-      <Ticket />
+      {isTicketWon ? (
+        <Result />
+      ) : (
+        <Ticket isTicketWon={isTicketWon} setIsTicketWon={setIsTicketWon} />
+      )}
     </div>
   );
 };
