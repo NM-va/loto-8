98ce1d431eeb2e77bb5b435fd8c38a802ad263f0
NM-va <nad.n.mwa@gmail.com>
2022-07-17 19:48:14 +0300

    add Filed

--- src/components/ticket/Ticket.tsx
+++ src/components/ticket/Ticket.tsx
@@ -1,105 +1,119 @@
-import React, { useState } from 'react';
+import React, { FC, useState } from 'react';
 
 import { ImMagicWand } from 'react-icons/im';
 
+import { appAPI, SelectedNumberType } from '../../api/app-api';
+import {
+  COUNT_MATCH_FIRST_FIELD,
+  COUNT_MATCH_FIRST_FIELD_SECOND_CASE,
+  COUNT_MATCH_SECOND_FIELD,
+  COUNT_RETRY,
+  INTERVAL_RETRY,
+  MAX_COUNT_SELECTED_FIRST_FIELD,
+  MAX_COUNT_SELECTED_SECOND_FIELD,
+  NUMBER_CELL_FIRST_FIELD,
+  NUMBER_CELL_SECOND_FIELD,
+} from '../../constants/constants';
+import { arrayComparison } from '../../utils/arrayComparison';
 import { fillArray } from '../../utils/fillArray';
+import { getArrayRandomNumbers } from '../../utils/getArrayRundomNumbers';
 
-import { Cell } from './Cell';
+import { Field } from './Field';
 
-export const Ticket = (): any => {
-  const [selectedElementsFirstFiled, setSelectedElementsFirstFiled] = useState<number[]>(
-    [],
-  );
-  const [selectedElementsSecondFiled, setSelectedElementsSecondFiled] = useState<
-    number[]
-  >([]);
+type PropsType = {
+  isTicketWon: boolean;
+  setIsTicketWon: (isWon: boolean) => void;
+};
 
-  const MAX_COUNT_SELECTED_FIRST_FIELD = 8;
-  const MAX_COUNT_SELECTED_SECOND_FIELD = 2;
-  const NUMBER_CELL_FIRST_FIELD = 19;
-  const NUMBER_CELL_SECOND_FIELD = 2;
-
-  const toggleSelectedCellFirstField = (numberCell: number): void => {
-    const isSelected = selectedElementsFirstFiled.includes(numberCell);
-
-    if (
-      selectedElementsFirstFiled.length === MAX_COUNT_SELECTED_FIRST_FIELD &&
-      !isSelected
-    ) {
-      return;
-    }
-
-    if (isSelected) {
-      const arrSelectedElements = selectedElementsFirstFiled.filter(
-        selectedNumber => selectedNumber !== numberCell,
-      );
-
-      setSelectedElementsFirstFiled(arrSelectedElements);
-    } else {
-      setSelectedElementsFirstFiled([...selectedElementsFirstFiled, numberCell]);
-    }
-  };
-
-  const toggleSelectedCellSecondField = (numberCell: number): void => {
-    const isSelected = selectedElementsSecondFiled.includes(numberCell);
-
-    if (
-      selectedElementsSecondFiled.length === MAX_COUNT_SELECTED_SECOND_FIELD &&
-      !isSelected
-    ) {
-      return;
-    }
-
-    if (isSelected) {
-      const arrSelectedElements = selectedElementsSecondFiled.filter(
-        selectedNumber => selectedNumber !== numberCell,
-      );
-
-      setSelectedElementsSecondFiled(arrSelectedElements);
-    } else {
-      setSelectedElementsSecondFiled([...selectedElementsSecondFiled, numberCell]);
-    }
-  };
+export const Ticket: FC<PropsType> = ({ isTicketWon, setIsTicketWon }) => {
+  const [firstField, setSelectedElementsFirstFiled] = useState<number[]>([]);
+  const [secondField, setSelectedElementsSecondFiled] = useState<number[]>([]);
 
   const arrayNumbersFirstField = fillArray(NUMBER_CELL_FIRST_FIELD);
   const arrayNumbersSecondField = fillArray(NUMBER_CELL_SECOND_FIELD);
+
+  let arrForFirstField: number[] = [];
+  let arrForSecondField: number[] = [];
+  let countQueries = 0;
+
+  const selectedNumber: SelectedNumberType = { firstField, secondField };
+
+  const onMakeRandomArrays = (): void => {
+    arrForFirstField = getArrayRandomNumbers(
+      MAX_COUNT_SELECTED_FIRST_FIELD,
+      NUMBER_CELL_FIRST_FIELD,
+    );
+
+    arrForSecondField = getArrayRandomNumbers(
+      MAX_COUNT_SELECTED_SECOND_FIELD,
+      NUMBER_CELL_SECOND_FIELD,
+    );
+  };
+
+  const sendData = async (): Promise<void> => {
+    try {
+      await appAPI.addFieldsData({ selectedNumber, isTicketWon });
+    } catch (e: any) {
+      setTimeout(() => {
+        if (countQueries < COUNT_RETRY) {
+          sendData();
+          countQueries += 1;
+        } else throw new Error(e);
+      }, INTERVAL_RETRY);
+    }
+  };
+
+  const calcWinner = (): void => {
+    const countMatchForFirstField = arrayComparison(firstField, arrForFirstField);
+    const countMatchForSecondField = arrayComparison(secondField, arrForSecondField);
+
+    if (countMatchForFirstField > COUNT_MATCH_FIRST_FIELD) {
+      setIsTicketWon(true);
+    } else if (
+      countMatchForFirstField > COUNT_MATCH_FIRST_FIELD_SECOND_CASE &&
+      countMatchForSecondField === COUNT_MATCH_SECOND_FIELD
+    ) {
+      setIsTicketWon(true);
+    }
+  };
+
+  const handleSubmit = (): void => {
+    calcWinner();
+    sendData();
+    setSelectedElementsFirstFiled([]);
+    setSelectedElementsSecondFiled([]);
+  };
 
   return (
     <div className="card">
       <div className="cardHeader">
         <div className="titleCard">Билет 1</div>
-        <button type="button" className="btnDefault">
+        <button type="button" className="btnDefault" onClick={onMakeRandomArrays}>
           <ImMagicWand />
         </button>
       </div>
-      <div className="fieldOne">
+      <Field
+        arrayNumbers={arrayNumbersFirstField}
+        selectedElements={firstField}
+        setSelectedElements={setSelectedElementsFirstFiled}
+        maxCountSelected={MAX_COUNT_SELECTED_FIRST_FIELD}
+      >
         <div className="titleField">
           <span className="textBold">Поле 1</span> Отметьте 8 чисел.
         </div>
-        {arrayNumbersFirstField.map(number => (
-          <Cell
-            numberCell={number}
-            key={`${number} "nad"`}
-            changeSelectedCell={() => toggleSelectedCellFirstField(number)}
-            isSelectedCell={selectedElementsFirstFiled.includes(number)}
-          />
-        ))}
-      </div>
-      <div className="fieldSecond">
+      </Field>
+      <Field
+        arrayNumbers={arrayNumbersSecondField}
+        selectedElements={secondField}
+        setSelectedElements={setSelectedElementsSecondFiled}
+        maxCountSelected={MAX_COUNT_SELECTED_SECOND_FIELD}
+      >
         <div className="titleField">
           <span className="textBold">Поле 2</span> Отметьте 1 число.
         </div>
-        {arrayNumbersSecondField.map(number => (
-          <Cell
-            numberCell={number}
-            key={`${number} "nad"`}
-            changeSelectedCell={() => toggleSelectedCellSecondField(number)}
-            isSelectedCell={selectedElementsSecondFiled.includes(number)}
-          />
-        ))}
-      </div>
+      </Field>
       <div className="btnBox">
-        <button type="submit" className="btnOutline">
+        <button type="submit" className="btnOutline" onClick={handleSubmit}>
           Показать результат
         </button>
       </div>
